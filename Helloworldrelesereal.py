4c5017f734b219310e7584eca0b534c2a0a1d9d9
52zagain52-blip <52zagain52@gmail.com>
2026-07-08 21:52:26 +0700

    Create Helloworldrelesereal.py

---
+++ Helloworldrelesereal.py
@@ -0,0 +1,23 @@
+import random
+print("H   H EEEEE L     L     OOO   W   W OOO  RRR  L     DDDD") 
+print("H   H E     L     L    O   O  W W O   O R R  L     D   D")       
+print("HHHHH EEEEE L     L    O   O  WWW O   O RRR  L     D   D") 
+print("H   H E     L     L    O   O  W W O   O R R  L     D   D") 
+print("H   H EEEEE LLLLL LLLLL OOO   W W OOO  R R  LLLLL DDDD") 
+x = 0
+while True:
+    print("это улучшеная версия hello world ") 
+    y = int(input("...") 
+    if y = 1:
+        print("hello world") 
+    elif y = 2:
+        QWERTY = [" hello world, goodbye Mars"]
+        XYZ = random.choice(QWERTY) 
+        print ("XYZ") 
+    elif y = 3:
+        brake
+    else:
+        print("1 - просто HW") 
+        print("2 - рандом между GM и  HW") 
+        print("3 - выход") 
+        
